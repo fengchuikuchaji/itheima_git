f03b927b625b110ae634da5d89b98717032bd654
leevi <348390180@qq.com>
2020-09-19 22:20:25 +0800

    firstCommit

---
+++ src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,28 @@
+package com.itheima.pojo;
+
+/**
+ * 包名:com.itheima.pojo
+ *
+ * @author Leevi
+ * 日期2020-09-19  22:16
+ */
+public class User {
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
