e8bdbec1d4108e66bee11e1de98270ad2204761e
leevi <348390180@qq.com>
2020-09-19 22:35:28 +0800

    four

---
+++ src/main/java/com/itheima/pojo/Helloworld.java
@@ -0,0 +1,13 @@
+package com.itheima.pojo;
+
+/**
+ * 包名:com.itheima.pojo
+ *
+ * @author Leevi
+ * 日期2020-09-19  22:34
+ */
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
